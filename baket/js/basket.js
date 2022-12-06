9d9cd7b29f4dd11c595d90bf2cd4b3fa5d0f37e0
NatigAgarzayev <116652802+NatigAgarzayev@users.noreply.github.com>
2022-12-06 23:58:16 +0400

    With cart page

--- baket/js/basket.js
+++ baket/js/basket.js
@@ -1,1 +1,21 @@
-console.log(basketItems);
+const cart = document.querySelector('.cart__body');
+let getBasketItems = JSON.parse(localStorage.getItem('basket'));
+let sum = 0;
+getBasketItems.forEach(item => {
+    sum += +item.price * item.count;
+    cart.innerHTML +=
+    `
+        <div class="cart__item">
+            <div class="cart__name">${item.name}</div>
+            <div class="cart__price">$${item.price}</div>
+            <div class="cart__count">Count: ${item.count}</div>
+        </div>
+    `
+});
+cart.innerHTML += 
+`
+    <div class="cart__item">
+        <div class="cart__overall">Total Amount:</div>
+        <div class="cart__total">$${sum} + taxes</div>
+    </div>
+`
--- baket/js/main.js
+++ baket/js/main.js
@@ -2,13 +2,18 @@ const cards = document.querySelector('.cards'),
 basket = document.querySelector('.basket'),
 counter = document.querySelector('.count');
 const products = [ { id: 1, name: 'Black', price: 2 }, { id: 2, name: 'Latte', price: 5 }, { id: 3, name: 'Espresso', price: 6 }, { id: 4, name: 'Eappucino', price: 4 }, { id: 5, name: 'Lungo', price: 3 }, { id: 6, name: 'Macchiato', price: 6 } ];
-const basketItems = [];
+let basketItems = [];
+counter.innerText = 0;
 let quant = 0;
 let count = 0;
 localStorage.setItem('products', JSON.stringify(products));
+let basketItemsSave = JSON.parse(localStorage.getItem('basket'));
+if(basketItemsSave !== null){
+    basketItems = basketItemsSave;
+}
 
 getProducts();
-
+getBasket();
 function createCard(id, name, price){
     let productCard = document.createElement('div'),
         productName = document.createElement('h3'),
@@ -48,25 +53,31 @@ function createCard(id, name, price){
             }
         }    
         count = 0;
-        localStorage.setItem('basket', JSON.stringify(basketItems));
         
         for(let i = 0; i < basketItems.length; i++){
             quant += basketItems[i].count;
         }
         counter.innerText = quant;
+        localStorage.setItem('basket', JSON.stringify(basketItems));
     });
 }
 
+
 function getProducts(){
-    quant = 0;
     let getProducts = JSON.parse(localStorage.getItem('products'));
-    let getBasketCount = JSON.parse(localStorage.getItem('basket'));
+    let getBasket = JSON.parse(localStorage.getItem('basket'));
     getProducts.forEach(product => {
         createCard(product.id, product.name, product.price)
-        localStorage.setItem('basket', JSON.stringify(basketItems));
+        localStorage.setItem('basket', JSON.stringify(getBasket));
     });
-    for(let i = 0; i < getBasketCount.length; i++){
-        quant += getBasketCount[i].count;
+    
+}
+
+function getBasket(){
+    quant = 0;
+    let getBasket = JSON.parse(localStorage.getItem('basket'));
+    for(let i = 0; i < getBasket.length; i++){
+        quant += getBasket[i].count;
     }
     counter.innerText = quant;
 }
